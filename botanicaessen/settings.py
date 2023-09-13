464231e781217d47b70fa0f24e451b0c5081cdf1
Sofia-Bourdon <>
2023-09-13 10:56:29 +0000

    added a new products app and models

--- botanicaessen/settings.py
+++ botanicaessen/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'boutique',
+    'products',
 ]
 
 MIDDLEWARE = [
---
+++ products/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils.text import slugify
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    sku = models.CharField(max_length=255, unique=True)
+    image_url = models.URLField(max_length=1024, blank=True, null=True)
+    image = models.ImageField(
+        upload_to='products/images/', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
