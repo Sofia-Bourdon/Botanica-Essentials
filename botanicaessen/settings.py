96ad3f15eb1106ec06cf5c1837ca7014f57cf63a
Sofia-Bourdon <>
2023-10-01 14:33:46 +0000

    developing front page and fixing main page image

--- botanicaessen/settings.py
+++ botanicaessen/settings.py
@@ -92,6 +92,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.static',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
@@ -175,7 +176,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
