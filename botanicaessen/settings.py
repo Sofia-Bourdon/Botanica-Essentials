dec419b828735f166008572c974bc1ac161c541d
Sofia-Bourdon <>
2023-10-04 10:33:22 +0000

    added views and templates 'contact' (user contact form)

--- botanicaessen/settings.py
+++ botanicaessen/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'widget_tweaks',
     'cloudinary',
     'home',
     'products',
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.http import HttpResponse
+from django.contrib import messages
 
-# Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the form data to the database
+            messages.success(
+                request, 'Your message has been sent successfully!')
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact_us.html', {'form': form})
+
