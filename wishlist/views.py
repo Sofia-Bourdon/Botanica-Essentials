e6e5dcbb34a01f9b26aae70903d61f6c15acf3c4
Sofia-Bourdon <>
2023-10-12 21:20:24 +0000

    added feedback messages to wishlist views

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from products.models import Product
 
 
 def view_wishlist(request):
     """ A view that renders the wishlist page """
+
+    if not request.user.is_authenticated:
+        messages.info(request, 'You need to be logged in to add view your wishlist.')
+        return redirect(reverse('account_login'))
+
     return render(request, 'wishlist/wishlist.html')
 
 
 def add_to_wishlist(request, item_id):
     """ Add a product to the wishlist """
+
+    if not request.user.is_authenticated:
+        messages.info(
+            request, 'You need to be logged in to add items to your wishlist.')
+        return redirect(reverse('account_login'))
 
     product = get_object_or_404(Product, pk=item_id)
     wishlist = request.session.get('wishlist', {})
@@ -27,6 +38,11 @@ def add_to_wishlist(request, item_id):
 
 def remove_from_wishlist(request, item_id):
     """ Remove a product from the wishlist """
+
+    if not request.user.is_authenticated:
+        messages.info(
+            request, 'You need to be logged in to remove items from your wishlist.')
+        return redirect(reverse('account_login'))
 
     product = get_object_or_404(Product, pk=item_id)
     wishlist = request.session.get('wishlist', {})
