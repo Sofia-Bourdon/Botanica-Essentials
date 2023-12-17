e9e32d57b0240ee00f46668ceff127d80f23d68c
Sofia-Bourdon <>
2023-12-17 22:23:38 +0000

    adding a new wishlist model and running migrations

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
+from user.models import UserProfile
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'product')
+
+    def __str__(self):
+        return f"{self.user}'s wishlist item: {self.product}"
