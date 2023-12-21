26709eafff1d58a6abffc1c51d41c1496aa66795
Sofia-Bourdon <>
2023-12-21 13:13:45 +0000

    added new post model to blog and migratred to db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=False)
+    body = models.TextField()
+
