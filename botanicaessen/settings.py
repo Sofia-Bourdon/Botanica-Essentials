36d86ad0cef5a92b170e7df812c315b899b763e6
Sofia-Bourdon <>
2023-12-21 09:08:38 +0000

    connected urls and created new template for the blog app

--- botanicaessen/settings.py
+++ botanicaessen/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
     'wishlist',
     'user',
     'checkout',
+    'blog',
     'crispy_forms',
 ]
 
--- botanicaessen/urls.py
+++ botanicaessen/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path('checkout/', include('checkout.urls')),
     path('user/', include('user.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'home.views.custom_404'
