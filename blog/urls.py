7b69fdb4fdf42ae4204f33b9868aeacfea4f43db
Sofia-Bourdon <>
2023-12-23 21:28:11 +0000

    added new delete post view, and delete button to post inside blog template

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from .views import PostList
 
 urlpatterns = [
     path('blog/', PostList.as_view(), name='blog'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
 from .models import Post
 
@@ -6,3 +8,14 @@ class PostList(ListView):
     model = Post
     template_name = 'blog/blog.html' 
     context_object_name = 'posts'
+
+@login_required
+def delete_post(request, post_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
+    messages.success(request, 'Post deleted sucessfully!')
+    return redirect(reverse('blog'))
