0ae1cd9b0d3348fa9ad3ec770df3559a726cd604
Sofia-Bourdon <>
2023-12-25 14:32:15 +0000

    installed whitenoise

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .views import PostList
+from .views import BlogList
 
 urlpatterns = [
-    path('blog/', PostList.as_view(), name='blog'),
-    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('', BlogList.as_view(), name='blog'),
+    path('delete/<int:post_id>/', views.delete_blog, name='delete_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,14 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
 from .models import Post
 
-class PostList(ListView):
+class BlogList(ListView):
     model = Post
     template_name = 'blog/blog.html' 
     context_object_name = 'posts'
 
 @login_required
-def delete_post(request, post_id):
-    """ Delete a product from the store """
+def delete_blog(request, post_id):
+    """ Delete a blog from the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
