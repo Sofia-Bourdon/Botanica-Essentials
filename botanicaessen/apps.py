09be236a4828477d8fc0b16e15c035be123b2873
Sofia-Bourdon <>
2024-01-01 22:18:21 +0000

    adding new apps config in attempt to solve RuntimeError found in testing

---
+++ botanicaessen/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class botanicaessenConfig(AppConfig):
+    name = 'botanicaessen'
+    verbose_name = 'A Much Better Name'
--- botanicaessen/settings.py
+++ botanicaessen/settings.py
@@ -49,6 +49,7 @@ ALLOWED_HOSTS = [
 ]
 
 INSTALLED_APPS = [
+    'botanicaessen.apps.botanicaessenConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
